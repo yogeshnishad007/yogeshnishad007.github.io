da1d466d6488136a93df4e6613ff52513f35303a
yogeshnishad007 <yogiatoy27518@gmail.com>
2023-03-01 10:03:44 +0530

    Updates

--- static/js/449.4b973f7e.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkmy_portfolio=self.webpackChunkmy_portfolio||[]).push([[449],{8449:function(o,e,f){f.r(e),e.default={}}}]);
-//# sourceMappingURL=449.4b973f7e.chunk.js.map
---
+++ static/js/449.d8b25b6d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_portfolio=self.webpackChunkmy_portfolio||[]).push([[449],{449:function(o,e,f){f.r(e),e.default={}}}]);
+//# sourceMappingURL=449.d8b25b6d.chunk.js.map
